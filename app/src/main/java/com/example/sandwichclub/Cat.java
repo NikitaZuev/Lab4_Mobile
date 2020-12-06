2e737ce4c2b1862e1792578d2bb3be9cb4a6e1f2
Kalpon Gilop <bridgez712@gmail.com>
2020-12-06 20:32:44 +0700

    Класс Cat и конвертация экземпляра в JSON

--- app/src/main/java/com/example/sandwichclub/Cat.java
+++ app/src/main/java/com/example/sandwichclub/Cat.java
@@ -1,4 +1,26 @@
 package com.example.sandwichclub;
 
+import android.graphics.Color;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
 public class Cat {
+    public String name; // имя
+    public int age; // возраст
+    public int color; // цвет
+
+    // Конструктор
+    public Cat(){
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.BLACK;
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
+    }
 }
